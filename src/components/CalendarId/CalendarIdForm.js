3df8d34eebd6fcb46ca801c23011d5fa41aa846d
Remzi <ict@remzi.info>
2019-11-12 17:29:54 +0100

    CalendarIdForm formating and documentation

--- src/components/CalendarId/CalendarIdForm.js
+++ src/components/CalendarId/CalendarIdForm.js
@@ -9,33 +9,36 @@ export default class CalendarIdForm extends React.Component {
       
       <div className="form-container">
         <form className="form" onSubmit={this.props.onSubmit}>
-          <h2>Setup calendar id</h2>
-          <p>Copy paste the following calendar id:</p>
+          <h2>Setup Google Calendar - Calendar ID</h2>
+          <p>You can find the calendar ID from the Google calendar under "Calendar settings" and down under section "Integrate calendar"</p>
+
           {!this.props.calendar ? 'loading...' :
           <ul> {this.props.calendar.map(calendar => {
             return <li key={calendar.id}>{calendar.id}</li>
           })}</ul>}
 
-          <label>
-            Calendar id:
-          <input
-            type='text'
-            value={this.props.calendar_id}
-            name='calendar_id'
-            placeholder='calendar id'
-            onChange={this.props.onChange}
-            />
-          </label>
-          <label>
-            Password:
+          <p>
+            <label>Calendar ID:</label><br/>
             <input
-            type='password'
-            value={this.props.password}
-            name='password'
-            placeholder='enter password'
-            onChange={this.props.onChange}
-            />
-          </label>
+              type='text'
+              value={this.props.calendar_id}
+              name='calendar_id'
+              placeholder='Enter calendar ID'
+              onChange={this.props.onChange}
+            /><br/><span>(Example: example@gmail.com or codaisseur.com_repcdhtcgm95695p0nj15j21dg@group.calendar.google.com)</span>
+          </p>
+          
+          <p>
+            <label>Password:</label><br/>
+            <input
+              type='password'
+              value={this.props.password}
+              name='password'
+              placeholder='Enter password'
+              onChange={this.props.onChange}
+            /> <span>(Admin password for validation)</span>
+          </p>
+          
           <button type='submit'>Submit calendar id</button>
         </form>
       </div>
