b66e0798f3967a45a0f816db4e9ce7b6a1e02ab8
Remzi <ict@remzi.info>
2019-11-12 15:43:32 +0100

    ConfigForm formating and documentation

--- src/components/Config/ConfigForm.js
+++ src/components/Config/ConfigForm.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export default class ConfigForm extends React.Component {
 
   render(){
@@ -11,38 +10,42 @@ export default class ConfigForm extends React.Component {
       <h2>Setup configuration</h2>
       <p>If you setup your service account you get a client email and a private key.</p>
       <p>You need both, plus your own password for this website to make a connection with the Google Calendar Api</p>
-
-        <label>
-          Email:
-          <input 
-          type='text' 
-          value={this.props.client_email} 
-          name='client_email' 
-          placeholder='enter email' 
-          onChange={this.props.onChange} 
-          />
-        </label>
-        <label>
-          Private key:
-          <input
+      <p><strong>You can get all the information from the .json file you got earlier. Copy what's between the double qoutes and paste them here.</strong></p>
+      
+      <p>
+        <label>Client email:</label><br/>
+        <input
+          type='text'
+          value={this.props.client_email}
+          name='client_email'
+          placeholder='Enter client email'
+          onChange={this.props.onChange}
+        /> <span>(Example: admin-calendar@example-library-2712.iam.gserviceaccount.com)</span>
+      </p>
+      
+      <p>
+        <label>Private key:</label><br/>
+        <input
           type='text'
           value={this.props.private_key}
           name='private_key'
           placeholder='enter key'
           onChange={this.props.onChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
+        /> <span>(-----BEGIN PRIVATE KEY-----............-----END PRIVATE KEY-----\n)</span>
+      </p>
+      
+      <p>
+        <label>Password:</label><br/>
+        <input
           type='password'
           value={this.props.password}
           name='password'
           placeholder='enter password'
           onChange={this.props.onChange}
-          />
-        </label>
-        <button type='submit'>Submit configuration</button>
+        /> <span>(Admin password for validation)</span>
+      </p>
+      
+      <button type='submit'>Submit configuration</button>
       </form> 
     </div>
   )}
