407cb7c54a3b436b996d96ce59bdade44860fcb7
Remzi <ict@remzi.info>
2019-11-13 16:25:27 +0100

    Improved formatting

--- src/components/Signup/SignupForm.js
+++ src/components/Signup/SignupForm.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class SignupForm extends React.Component {
   
-  render() {
-    return <form onSubmit={this.props.onSubmit}>
-      <h2>Sign up</h2>
-      <p>
-        <label>Email:</label><br/>
-        <input
-          type='text'
-          value={this.props.email}
-          name='email'
-          placeholder='enter email'
-          onChange={this.props.onChange}
-        />
-      </p>
-      
-      <button type='submit'>Sign up</button>
-    </form>
+  render(props) {
+    return <>
+      <Link to={'/'}>Go back</Link>
+      <form onSubmit={this.props.onSubmit}>
+        <h2>Sign up</h2>
+        <p>
+          <label>Email:</label><br/>
+          <input
+            type='text'
+            value={this.props.email}
+            name='email'
+            placeholder='enter email'
+            onChange={this.props.onChange}
+          />
+        </p>
+        <button type='submit'>Sign up</button>
+      </form>
+    </>
   }
 }
