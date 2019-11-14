4842f37ab52fcbc7773048cd62352d11674bfd5d
Remzi <ict@remzi.info>
2019-11-14 14:12:06 +0100

    Small changes

--- src/components/Login/LoginFormContainer.js
+++ src/components/Login/LoginFormContainer.js
@@ -6,42 +6,42 @@ import LoginForm from './LoginForm'
 // import lscache from 'lscache'
 
 class LoginFormContainer extends React.Component {
-    state = {
-        email: '',
-        password: ''
-    }
+  state = {
+    email: '',
+    password: ''
+  }
 
-    onChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
-    onSubmit = (event) => {
-        event.preventDefault()
-        this.props.login(this.state.email, this.state.password)
-    }
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.login(this.state.email, this.state.password)
+  }
 
-    render() {
-        // const JWT = lscache.get('JWT')
-        // if(JWT) {
-        //     return <Redirect to='/overview'/>
-        // }
+  render() {
+    // const JWT = lscache.get('JWT')
+    // if(JWT) {
+    //     return <Redirect to='/overview'/>
+    // }
 
-        return <LoginForm
-            onSubmit={this.onSubmit}
-            onChange={this.onChange}
-            values={this.state}
-            user = {this.props.user}
-        />
-    }
+    return <LoginForm
+      onSubmit = {this.onSubmit}
+      onChange = {this.onChange}
+      values = {this.state}
+      user = {this.props.user}
+    />
+  }
 }
 
 const mapStateToProps = (reduxState) => {
-    // console.log('Redux state', reduxState);
-    return {
-      user: reduxState.user
-    }
+  // console.log('Redux state', reduxState);
+  return {
+    user: reduxState.user
+  }
 }
 
 export default connect(mapStateToProps, {login})(LoginFormContainer);
