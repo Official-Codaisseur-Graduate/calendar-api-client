9ae0e40f2b5cb75a331f764a80d1b98a83e4e685
Simona Winnekes <simona.winnekes@googlemail.com>
2019-12-04 17:11:46 +0100

    Add basic homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import CodaisseurLogo from '../../assets/codaisseur-logo.png';
+import { Link } from 'react-router-dom';
 
 export default function HomePage(props) {
-    return <div></div>;
+    return (
+        <div className="main-home">
+            <img
+                alt="Codaisseur Academy Calendar"
+                src={CodaisseurLogo}
+                className="main-logo"
+            />
+            <h1>The Academy Calendar</h1>
+            <p>
+                The app is made to show the events happening on certain dates of
+                the calendar. In short, once a user clicks on a day, the events
+                for that particular date would be displayed. The user and admin
+                stories are described below.
+            </p>
+            <Link to="/login" className="btn btn-danger">
+                Login
+            </Link>{' '}
+            <Link to="/signup" className="btn btn-danger">
+                Signup
+            </Link>
+        </div>
+    );
 }
