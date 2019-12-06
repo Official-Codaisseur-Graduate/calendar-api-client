3824a5314bda7ddaf419cf2f8f3f6030326ab2d3
krik-chry <krikonis.chrisostomos@gmail.com>
2019-12-06 16:01:29 +0100

    Assistant button

--- src/components/CodaisseurCalendar/CodaisseurCalendarContainer.js
+++ src/components/CodaisseurCalendar/CodaisseurCalendarContainer.js
@@ -9,69 +9,75 @@ import moment from 'moment';
 import lscache from 'lscache';
 
 class CodaisseurCalendarContainer extends Component {
-    user = lscache.get('user');
-    state = {
-        selectedEvent: null,
-    };
+  user = lscache.get('user');
+  state = {
+    selectedEvent: null,
+    assistantRequest: false
+  };
 
-    onSelectEvent = event => {
-        this.setState({ selectedEvent: event });
-    };
+  onSelectEvent = event => {
+    this.setState({ selectedEvent: event });
+  };
 
-    onSelectSlot = slot => {
-        console.log(slot);
-    };
+  onSelectSlot = slot => {
+    console.log(slot);
+  };
 
-    onNavigate = date => {
-        this.setState({ selectedEvent: null });
-        const transformedDate = moment(date);
-        console.log(transformedDate);
-        const year = transformedDate.year();
-        const month = transformedDate.month();
-        this.props.selectDate(year, month);
-        this.props.fetchEvents(year, month);
-    };
+  onNavigate = date => {
+    this.setState({ selectedEvent: null });
+    const transformedDate = moment(date);
+    console.log(transformedDate);
+    const year = transformedDate.year();
+    const month = transformedDate.month();
+    this.props.selectDate(year, month);
+    this.props.fetchEvents(year, month);
+  };
 
-    componentDidMount = () => {
-        const now = moment();
-        const year = now.year();
-        const month = now.month();
-        this.props.selectDate(year, month);
-        this.props.fetchEvents(year, month);
-        this.props.fetchUsers();
-    };
+  componentDidMount = () => {
+    const now = moment();
+    const year = now.year();
+    const month = now.month();
+    this.props.selectDate(year, month);
+    this.props.fetchEvents(year, month);
+    this.props.fetchUsers();
+  };
 
-    closeEvent = () => {
-        this.setState({ selectedEvent: null });
-    };
-
-    render() {
-        return (
-            <div className="calendar-wrapper">
-                <CodaisseurCalendar
-                    onSelectEvent={this.onSelectEvent}
-                    onNavigate={this.onNavigate}
-                    onSelectSlot={this.onSelectSlot}
-                    events={this.props.events}
-                    selectedEvent={this.state.selectedEvent}
-                    closeEvent={this.closeEvent}
-                    beAssistant={this.props.beAssistant}
-                    user={this.user}
-                    users={this.props.users}
-                />
-            </div>
-        );
-    }
+  closeEvent = () => {
+    this.setState({ selectedEvent: null });
+  };
+  onAssistantRequest = (teacherMail, event) => {
+    this.props.beAssistant(teacherMail, event);
+    this.setState({
+      assistantRequest: true
+    });
+  };
+  render() {
+    return (
+      <div className='calendar-wrapper'>
+        <CodaisseurCalendar
+          onSelectEvent={this.onSelectEvent}
+          onNavigate={this.onNavigate}
+          onSelectSlot={this.onSelectSlot}
+          events={this.props.events}
+          selectedEvent={this.state.selectedEvent}
+          assistantRequest={this.state.assistantRequest}
+          closeEvent={this.closeEvent}
+          user={this.user}
+          users={this.props.users}
+        />
+      </div>
+    );
+  }
 }
 const mapStateToProps = reduxState => {
-    return {
-        events: reduxState.events,
-        users: reduxState.users,
-    };
+  return {
+    events: reduxState.events,
+    users: reduxState.users
+  };
 };
 export default connect(mapStateToProps, {
-    fetchEvents,
-    selectDate,
-    beAssistant,
-    fetchUsers,
+  fetchEvents,
+  selectDate,
+  beAssistant,
+  fetchUsers
 })(CodaisseurCalendarContainer);
--- src/components/CodaisseurCalendar/EventDetails.js
+++ src/components/CodaisseurCalendar/EventDetails.js
@@ -3,71 +3,64 @@ import { Button, Card } from 'react-bootstrap';
 import Moment from 'react-moment';
 
 export default function EventDetails(props) {
-    console.log(props.event);
+  console.log(props.event);
 
-    const allUsers = props.users;
-    const event = props.event;
-    console.log('allusers', allUsers);
-    console.log('event', event);
-    let teacher;
-    let assistant;
+  const allUsers = props.users;
+  const event = props.event;
+  console.log('allusers', allUsers);
+  console.log('event', event);
+  let teacher;
+  let assistant;
 
-    if (event.teacher) {
-        teacher = allUsers.find(
-            user => user.email === props.event.teacher.email
-        );
-    }
+  if (event.teacher) {
+    teacher = allUsers.find(user => user.email === props.event.teacher.email);
+  }
 
-    if (event.assistant) {
-        assistant = allUsers.find(
-            user => user.email === props.event.assistant.email
-        );
-    }
-
-    console.log('teacher', teacher);
-    console.log('assistant', assistant);
-    return (
-        <div className="">
-            <Card
-                bg="danger"
-                text="white"
-                style={{ width: '20rem' }}
-                className="event-details"
-            >
-                <Card.Header>
-                    <Moment format="DD. MMMM YYYY">{props.event.start}</Moment>
-                    <br />
-                    <Moment format="hh:mm">{props.event.start}</Moment> -
-                    <Moment format="hh:mm">{props.event.end}</Moment>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Title>{props.event.title}</Card.Title>
-                    <Card.Text>
-                        {props.event.description}
-                        {teacher && <span>Teacher: {teacher.name}</span>}
-
-                        {assistant && <span>Assistant: {assistant.name}</span>}
-                    </Card.Text>
-                    <nav className="buttons">
-                        {props.user.rank === 2 && (
-                            <Button
-                                variant="success"
-                                onClick={() =>
-                                    props.beAssistant(teacher.email, event)
-                                }
-                            >
-                                Want to assist?
-                            </Button>
-                        )}
-                        <Button
-                            onClick={() => props.closeEvent()}
-                            variant="light"
-                        >
-                            Close the event
-                        </Button>
-                    </nav>
-                </Card.Body>
-            </Card>
-        </div>
+  if (event.assistant) {
+    assistant = allUsers.find(
+      user => user.email === props.event.assistant.email
     );
+  }
+
+  console.log('teacher', teacher);
+  console.log('assistant', assistant);
+  return (
+    <div className=''>
+      <Card
+        bg='danger'
+        text='white'
+        style={{ width: '20rem' }}
+        className='event-details'
+      >
+        <Card.Header>
+          <Moment format='DD. MMMM YYYY'>{props.event.start}</Moment>
+          <br />
+          <Moment format='hh:mm'>{props.event.start}</Moment> -
+          <Moment format='hh:mm'>{props.event.end}</Moment>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>{props.event.title}</Card.Title>
+          <Card.Text>
+            {props.event.description}
+            {teacher && <span>Teacher: {teacher.name}</span>}
+
+            {assistant && <span>Assistant: {assistant.name}</span>}
+          </Card.Text>
+          <nav className='buttons'>
+            {props.user.rank === 2 && !props.assistantRequest && (
+              <Button
+                variant='success'
+                onClick={() => props.onAssistantRequest(teacher.email, event)}
+              >
+                Want to assist?
+              </Button>
+            )}
+            <Button onClick={() => props.closeEvent()} variant='light'>
+              Close the event
+            </Button>
+          </nav>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
